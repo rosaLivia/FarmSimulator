5d0c7c1613d7793f74cfdff78ef9552097fc9d3d
Vinicius-Marques6 <marquesv473@gmail.com>
2024-07-18 00:20:55 -0400

    feat: add basic header component

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,15 +1,59 @@
 <script>
 export default {
-
+    data() {
+        return {
+            price: 0
+        }
+    },
+    methods: {
+        formatCurrency(value) {
+            return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        }
+    }
 }
 </script>
 
 <template>
     <header>
-        <p>Header</p>
+        <!-- TODO: Logo -->
+        <h2>DrograFarma</h2>
+        <hr>
+        <input id="search-input" type="text" placeholder="O que deseja encontrar?">
+        <hr>
+        <div>
+            <!-- TODO: Icon -->
+            <p>Minha conta</p>
+            <p><b>Entrar</b></p>
+        </div>
+        <hr>
+        <div>
+            <!-- TODO: Icon -->
+            <p>Carrinho</p>
+            <p><b>{{ formatCurrency(price) }}</b></p>
+        </div>
     </header>
 </template>
 
-<style>
+<style scoped>
+    header {
+        background-color: white;
+        padding: 1em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1em;
+        border-bottom: .5em solid red;
+    }
 
+    #search-input {
+        padding: .75em;
+        font-size: 1em;
+        border: 1px solid #ccc;
+        border-radius: .5em;
+        min-width: 30em;
+    }
+
+    hr {
+        height: 2rem;
+    }
 </style>
