de51e604b4a5aa6ed05bc245ceda895ceed022cf
Guilherme Teles <151649638+guilherme-teles-dev@users.noreply.github.com>
2024-07-23 21:09:17 -0400

    fix: Products e Conexão com Banco

--- backend/config/database.php
+++ backend/config/database.php
@@ -85,10 +85,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'pgsql'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'sail'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
