e87f4975b5ce2d04314c5ddada728603aa4897fc
Guilherme Teles <151649638+guilherme-teles-dev@users.noreply.github.com>
2024-08-09 17:19:30 -0400

    feat: arquivos adicionais de gerenciamento de permissoes

---
+++ backend/app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Definicação:
+Arquivo responsável por criar comandos personalizáveis executáveis via prompt e agendar a excecução
+automática deles.
+
+Exemplos:
+Em tal hora, diáriamente, limpe todos os tokens criados.
+*/
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+  /**
+   * Define the application's command schedule.
+   */
+  protected function schedule(Schedule $schedule): void
+  {
+    $schedule->command('sanctum:prune-expired --hours=24')->dailyAt('15:10');
+    // $schedule->command('inspire')->hourly();
+  }
+
+  /**
+   * Register the commands for the application.
+   */
+  protected function commands(): void
+  {
+    $this->load(__DIR__ . '/Commands');
+
+    require base_path('routes/console.php');
+  }
+}
