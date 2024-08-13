461bdd9e7966255f1ce282d8c15766f4e8303b6c
rosaLivia <127208803+rosaLivia@users.noreply.github.com>
2024-08-12 20:59:49 -0400

    feat: add escopo inicial

--- frontend/pages/carrinho.vue
+++ frontend/pages/carrinho.vue
@@ -64,8 +64,12 @@
                             <td class="td" >{{ product.name }}</td>
                             <td class="td">{{ product.quantity }}</td>
                             <td class="td">
-                              <button @click="() => removeProduct(product.id)" class="text-red-500 button-icons">
+                              <button @click="() => addToCart(product)" class="text-orange-500 button-icons">
                                 <i class="pi pi-plus"></i>
+                              </button>
+                
+                              <button @click="() => removeProduct(product.id)" class="text-red-500 button-icons">
+                                
                                 <i class=" pi pi-minus"></i>
                               </button>
                             
@@ -131,65 +135,84 @@
               </div>
             </StepPanel>
         <StepPanel v-slot="{ activateCallback }" value="2">
-          <div class="flex mt-2 ml-5">
-                
-                <Card class="card-left bg-white-200 w-[600px]">
+         
+          <div class="grid place-items-center">
+                <Card class="card-center bg-white-400 w-[600px] h-[150px]">
                   <template #content>
-                    <p>Informações adicionais ou outro conteúdo pode ir aqui.</p>
+                    <p class="ml-40">Você está logado?</p>
+                    <NuxtLink to="/login" class="ml-40 hover:underline font-bold">Entrar </NuxtLink>
+                    <br>
+                    <br>
+                    <Button class="ml-40 bg-yellow-400 text-black font-semibold py-2" label="CONTINUAR PEDIDO" @click="activateCallback('3')" />
+                    
                   </template>
                 </Card>
 
-                
-                <div class="flex flex-col flex-grow gap-4 ml-5">
-                  <Card class="card bg-white-100">
-                    <template #title>
-                      <h2 class="font-semibold">RESUMO</h2>
-                    </template>
-                    <template #content>
-                      <div class="card-content">
-                        <fieldset>
-                          <div class="flex justify-between">
-                            <p class="text-left">Valor dos produtos:</p>
-                            <span class="ml-auto text-right font-semibold">R$ {{ totalProductValue }}</span>
-                          </div>
-                          <Divider />
-                          <div class="flex justify-between">
-                            <p class="text-left">Frete:</p>
-                            <span class="ml-auto text-right font-semibold">R$ 12,00</span>
-                          </div>
-                          <Divider />
-                          <div class="flex justify-between">
-                            <p class="text-left">Total:</p>
-                            <span class="ml-auto text-right font-semibold">R$ {{ totalValue }}</span>
-                          </div>
-                        </fieldset>
-                      </div>
-                    </template>
-                  </Card>
-                  <Card class="card-second bg-white">
-                    <template #content>
-                      <div class="card-content flex flex-col h-full justify-end">
-                        <Button class="mt-auto bg-yellow-400 text-black font-semibold py-2" label="CONTINUAR PEDIDO" @click="activateCallback('3')" />
-                      <div class="flex justify-center mt-auto">
-                        <NuxtLink to="/" class="hover:underline font-bold">Continuar comprando</NuxtLink>
-                      </div>
-                    </div>
-                    </template>
-                  </Card>
-                </div>
-              </div>
+          </div>      
+               
 
         </StepPanel>     
       
         <StepPanel v-slot="{ activateCallback }" value="3">
-          <div class="flex mt-2 ml-5">
+          
+                
+            <div class="flex mt-2 ml-5">
                 
                 <Card class="card-left bg-white-200 w-[600px]">
+    
                   <template #content>
-                    <p>Informações adicionais ou outro conteúdo pode ir aqui.</p>
+                    <h2 class="font-semibold" style="margin-bottom: 15px" >ENVIO DE ANÁLISE</h2>
+                    <div v-if="cart.length">
+                      <table class="w-full table-auto">
+                        <thead>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                          <th class="th">Quantidade</th>
+                          <th></th>
+                          <th class="th">Preço</th>
+                          <th></th>
+                       </thead>
+                      
+                     
+                        <tbody>
+                          
+                          <tr v-for="product in cart" :key="product.id" class="border-b">
+                          
+                            <td class="td"><img :src="product.image" class="product-image" /></td>
+                            <td class="td" >{{ product.name }}</td>
+                            <td class="td text-red-500 text-xs" style="font-size:11px;" >{{ product.prescription }}</td>
+                            <td class="td">{{ product.quantity }}</td>
+                            <td class="td">
+                              
+                              <button @click="() => addToCart(product)" class="text-orange-500 button-icons">
+                                <i class="pi pi-plus"></i>
+                              </button>
+                              <button @click="() => removeProduct(product.id)" class="text-orange-500 button-icons">
+                               
+                                <i class=" pi pi-minus"></i>
+                              </button>
+                              
+                            </td>
+                            <td class="td">R$ {{ product.valueTo.toFixed(2) }}</td>
+                            
+                           
+                            <td class="td">
+                              <button @click="() => removeProduct(product.id)" class="text-blue-500">
+                                <i class="pi pi-trash"></i>
+                              </button>
+                            </td>
+                           
+                          </tr>
+                         
+                        </tbody>
+                      </table>
+                    </div>
+                    <div v-else>
+                      <p>O carrinho está vazio</p>
+                    </div>
                   </template>
                 </Card>
-
                 <div class="flex flex-col flex-grow gap-4 ml-5">
                   <Card class="card bg-white-100">
                     <template #title>
@@ -216,6 +239,10 @@
                       </div>
                     </template>
                   </Card>
+
+
+
+
                   <Card class="card-second bg-white">
                     <template #content>
                       <div class="card-content flex flex-col h-full justify-end">
@@ -229,16 +256,56 @@
                 </div>
               </div>
 
+              <div class="flex justify-between items-center">
+
+                <Card class="card-pescription bg-white w-full">
+                  <template #content>
+                    <div class="flex items-center justify-between space-x-4">
+                    
+                      <p class="flex-shrink-0">Adicione a prescrição dos remédios:</p>
+
+                  
+                      <div class="flex-shrink-0"> <!--Componente de upload de arquivo prescrição-->
+                        <Toast />
+                        <FileUpload
+                          mode="basic"
+                          name="demo[]"
+                          url="/api/upload"
+                          accept="image/*"
+                          :maxFileSize="1000000"
+                          @upload="onUpload"
+                          :auto="true"
+                          chooseLabel="Enviar"
+                        />
+                      </div>
+                    </div>
+                  </template>
+                </Card>
+
+</div>
+
+
+
+
+
         </StepPanel>
     
         <StepPanel v-slot="{ activateCallback }" value="4">
           <div class="flex mt-2 ml-5">
                 
-                <Card class="card-left bg-white-200 w-[600px]">
+            <Card class="card-left bg-white-200 w-[600px] h-[300px]">
                   <template #content>
-                    <p>Informações adicionais ou outro conteúdo pode ir aqui.</p>
+                
+                    <Listbox 
+                      v-model="pagamentos" 
+                      :options="packages" 
+                      optionLabel="name" 
+                      class="w-full h-full "/>  
                   </template>
                 </Card>
+
+                          
+
 
                 
                 <div class="flex flex-col flex-grow gap-4 ml-5">
@@ -270,7 +337,7 @@
                   <Card class="card-second bg-white">
                     <template #content>
                       <div class="card-content flex flex-col h-full justify-end">
-                        <Button class="mt-auto bg-yellow-400 text-black font-semibold py-2" label="CONTINUAR PEDIDO" @click="activateCallback('5')" />
+                        <Button class="mt-auto bg-yellow-400 text-black font-semibold py-2" label="CONFIRMAR PEDIDO" @click="activateCallback('5')" />
                       <div class="flex justify-center mt-auto">
                         <NuxtLink to="/" class="hover:underline font-bold">Continuar comprando</NuxtLink>
                       </div>
@@ -317,6 +384,7 @@
                       </div>
                     </template>
                   </Card>
+                  
                   <Card class="card-second bg-white">
                     <template #content>
                       <div class="card-content flex flex-col h-full justify-end">
@@ -325,6 +393,7 @@
                         <NuxtLink to="/" class="hover:underline font-bold">Continuar comprando</NuxtLink>
                       </div>
                     </div>
+                    
                     </template>
                   </Card>
                 </div>
@@ -344,8 +413,9 @@
 
 <script setup>
   import Button from 'primevue/button';
+  import { useToast } from "primevue/usetoast";
   import { computed } from 'vue';
-  const {cart, products, removeProduct} = useCart();
+  const {cart, products, removeProduct, addToCart} = useCart();
   const totalProductValue = computed(() => {
   return cart.reduce((total, product) => total + (product.valueTo * product.quantity), 0).toFixed(2)
 })
@@ -356,6 +426,23 @@ const totalValue = computed(() => {
   return (parseFloat(totalProductValue.value) + shippingCost).toFixed(2)
 })
 
+const toast = useToast();
+
+const onUpload = () => {
+    toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+
+const pagamentos = ref();
+const packages = ref([
+    { name: 'PIX', code: 'PX' },
+    { name: 'CARTÃO DE CRÉDITO', code: 'CDC' },
+    { name: 'DINHEIRO', code: 'DD' },
+    { name: 'BITCOIN', code: 'BIT' },
+    { name: 'EURO', code: 'ER' },
+    { name: 'V-BUCKS', code: 'VB' },
+    { name: 'RIOT-POINTS', code: 'RP' },
+    { name: 'VALORANT POINTS', code: 'VP' }
+]);
 
 </script>
 
@@ -368,6 +455,15 @@ const totalValue = computed(() => {
 .card-second {
   @apply w-80 p-4 rounded-lg shadow-md;
 }
+.card-pescription {
+  
+  @apply w-[600px] p-4 rounded-lg shadow-md;
+  margin-top: 20px;
+  margin-left: 20px;
+  height: 100px;
+  padding-inline-start: 30px;
+}
+
 
 .card-content {
   @apply flex flex-col gap-2 h-full;
@@ -394,7 +490,7 @@ const totalValue = computed(() => {
 
 .td{
 
-
+  
   text-align: center;
   width: 200px;
   padding-top: 10px;
@@ -418,7 +514,10 @@ const totalValue = computed(() => {
 .border-b {
     border-bottom: 1px solid #ddd; 
   }
-
-
-
+  
+.th-vertical {
+    writing-mode: vertical-rl;
+    transform: rotate(180deg);
+    text-align: center;
+  }
 </style>
