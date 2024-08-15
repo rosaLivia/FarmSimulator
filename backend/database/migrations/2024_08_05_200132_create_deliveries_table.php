b4445676136fbc7151f526c9e7985b78f81d879f
Rafael Lima <rafael.lima.2021@hotmail.com>
2024-08-14 21:18:30 -0300

    correção erros na tabela cart e deliveries

--- backend/database/migrations/2024_08_05_200132_create_deliveries_table.php
+++ backend/database/migrations/2024_08_05_200132_create_deliveries_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('deliveries', function (Blueprint $table) {
             $table->id(); // Adiciona uma coluna de chave primária "id"
+            $table->string('deliveries'); // Adiciona uma coluna de chave primária "deliveries"
             $table->dateTime('arrived_date'); // Adiciona uma coluna "arrived_date" para armazenar a previsão de entrega
             $table->string('codigo'); // Adiciona uma coluna "codigo" do tipo string para armazenar o código de rastreio
             $table->timestamps(); // Adiciona colunas "created_at" e "updated_at"
--- backend/database/migrations/2024_08_05_200142_create_cart_table.php
+++ backend/database/migrations/2024_08_05_200142_create_cart_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
             $table->unsignedBigInteger('id_buy'); // Adiciona uma coluna "id_buy" do tipo inteiro sem sinal
             $table->unsignedBigInteger('id_drug'); // Adiciona uma coluna "id_drug" do tipo inteiro sem sinal
             $table->integer('quant'); // Adiciona uma coluna "quant" do tipo inteiro
-            $table->decimal('price', 8, 2); // Adiciona uma coluna "price" do tipo decimal com 8 dígitos totais e 2 casas decimais
+            $table->decimal('preco_unitario', 8, 2); // Adiciona uma coluna "price" do tipo decimal com 8 dígitos totais e 2 casas decimais
             $table->timestamps(); // Adiciona colunas "created_at" e "updated_at"
 
             // Definir chaves estrangeiras
-            $table->foreign('id_buy')->references('id')->on('buys')->onDelete('cascade');
-            $table->foreign('id_drug')->references('id')->on('drugs')->onDelete('cascade');
+            $table->foreign('id_buy')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('id_drug')->references('id')->on('products')->onDelete('cascade');
+            // o codigo abaixo está comentado pois a adição do preco do produto sera diretamente na tabela
+            //$table->foreign('preco_unitario')->references('price')->on('products')->onDelete('cascade');
         });
     }
 };
